255d3b18e935102344b96bd2b6061824d5d92e7c
Robin 'Ruadeil' Degen <mail@ruadeil.lgbt>
2025-07-06 14:29:38 +0200

    Rename --force-root to --i-did-not-ask to make it reusable for more overrides

--- alpaca/common/alpaca_application.py
+++ alpaca/common/alpaca_application.py
@@ -22,8 +22,8 @@ def _create_arg_parser_for_application(application_name: str) -> ArgumentParser:
                         help="Enable trace logging for debugging purposes This will disable the global exception "
                         "handler and will cause the application to crash on unhandled errors. ")
 
-    parser.add_argument("--force-root", action="store_true",
-                        help="Force the application to run as root, even if it is not recommended. "
+    parser.add_argument("--i-did-not-ask", action="store_true",
+                        help="Force the application to run as any user, even if it is not recommended. "
                              "Use with extreme caution, as this may lead to unexpected behavior.")
 
     return parser
@@ -70,11 +70,10 @@ def handle_main(application_name: str, require_root: bool, disallow_root: bool,
             raise PermissionError(f"Running '{application_name}' requires root privileges. Please run as root.")
 
         if disallow_root and is_root:
-            if not args.force_root:
+            if not args.i_did_not_ask:
                 raise PermissionError(f"Running '{application_name}' as root is not allowed. Please run as a normal user.")
             else:
-                logger.warning(f"Running '{application_name}' as root is not recommended. "
-                               "Use --force-root at your own risk.")
+                logger.warning(f"Running '{application_name}' as root is not recommended. Use at your own risk.")
 
         logger.debug("This software is provided under GNU GPL v3.0")
         logger.debug("This software comes with ABSOLUTELY NO WARRANTY")
