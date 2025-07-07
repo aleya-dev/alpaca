089b9cd12cda30ce0feeaf9a45112ad552439695
Robin 'Ruadeil' Degen <mail@ruadeil.lgbt>
2025-07-07 19:45:31 +0200

    Adjust sha files written and checked by write_file_hash and check_file_hash_from_file are now compatible with the shasum command

--- alpaca/common/hash.py
+++ alpaca/common/hash.py
@@ -1,15 +1,17 @@
 import hashlib
 import os
+from os.path import basename
+from pathlib import Path
 
 from alpaca.common.logging import logger
 
 
-def get_file_hash(path: str) -> str:
+def get_file_hash(path: str | Path) -> str:
     """
     Get the sha256 hash of a file
 
     Args:
-        path (str): The path to the file
+        path (str | Path): The path to the file
 
     Returns:
         str: The sha256 hash of the file
@@ -18,16 +20,21 @@ def get_file_hash(path: str) -> str:
         return hashlib.sha256(file.read()).hexdigest()
 
 
-def write_file_hash(path: str):
+def write_file_hash(path: str | Path):
     """
     Write the sha256 hash of a file to a file with a .sha256 extension
 
     Args:
-        path (str): The path to the file
-        hash_file_path (str): The path to write the hash to
+        path (str | Path): The path to the file
     """
+
+    filename = basename(path)
+
     with open(f"{path}.sha256", "w") as file:
         file.write(get_file_hash(path))
+        file.write("  ")
+        file.write(filename)
+        file.write("\n")
 
 
 def check_file_hash_from_string(path: str, expected_hash: str) -> bool:
@@ -54,12 +61,12 @@ def check_file_hash_from_string(path: str, expected_hash: str) -> bool:
     return True
 
 
-def check_file_hash_from_file(path: str) -> bool:
+def check_file_hash_from_file(path: str | Path) -> bool:
     """
     Check if a file exists and has the correct hash
 
     Args:
-        path (str): The path to the file
+        path (str | Path): The path to the file
 
     Returns:
         bool: True if the file exists and has the correct hash, False otherwise
@@ -71,6 +78,18 @@ def check_file_hash_from_file(path: str) -> bool:
         return False
 
     with open(sha_file_path, "r") as file:
-        expected_hash = file.read().strip()
+        line = file.read().strip()
+
+    parts = line.split("  ")
+
+    if len(parts) != 2:
+        logger.error(f"Hash file {sha_file_path} is malformed. Expected format: '<hash>  <filename>'.")
+        return False
+
+    expected_hash, filename = parts
+
+    if filename != basename(path):
+        logger.error(f"Hash file {sha_file_path} does not match the file {path}. Expected filename {filename}.")
+        return False
 
     return check_file_hash_from_string(path, expected_hash)
