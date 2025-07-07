f5737fa6a365b22951a2b34e5c2f6527d08a9dcf
Robin 'Ruadeil' Degen <mail@ruadeil.lgbt>
2025-07-08 00:01:09 +0200

    Improve config debug logging

--- alpaca/configuration/configuration.py
+++ alpaca/configuration/configuration.py
@@ -355,6 +355,12 @@ class Configuration:
             type_value = getattr(config, f"{key}_origin", None)
             config_type_str = _configuration_type_to_string(type_value) if type_value else "Unknown"
 
+            if value is None:
+                value = "None"
+
+            if isinstance(value, str):
+                value = f"\"{value}\""
+
             if isinstance(value, (list, tuple)):
                 logger.debug(f"{key.ljust(max_key_len)} = [ {' ' * (max_value_len - 2)}({config_type_str})")
                 for item in value:
