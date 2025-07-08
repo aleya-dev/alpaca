a4a12f79fe71a8b711f1905af00cb0b76be6c938
Robin 'Ruadeil' Degen <mail@ruadeil.lgbt>
2025-07-09 01:40:58 +0200

    Add support for reading the RecipeInfo from a PackageFile

--- alpaca/package_file.py
+++ alpaca/package_file.py
@@ -2,6 +2,9 @@ from tarfile import TarFile, open as tarfile_open
 from pathlib import Path
 from typing import Self
 
+from alpaca.package_file_info import FileInfo, read_file_info_from_string
+from alpaca.recipe_info import RecipeInfo
+
 
 class PackageFile:
     def __init__(self, package_path: str | Path | None = None):
@@ -18,3 +21,21 @@ class PackageFile:
 
     def _open(self):
         self._tar = tarfile_open(self.package_path, "r:gz")
+
+    def read_recipe_info(self) -> RecipeInfo:
+        """
+        Read the recipe info from the package file.
+
+        Returns:
+            str: The contents of the recipe info file.
+        """
+
+        if not self._tar:
+            self._open()
+
+        try:
+            with self._tar.extractfile(".recipe_info") as file:
+                return RecipeInfo.read_json_str(file.read().decode("utf-8"))
+        except KeyError:
+            raise FileNotFoundError("Recipe info file not found in the package.")
+
